a171dcf3685f038f6cb6d58995be23ac093d1c47
Adarsh Yadav <adyadav0807@gmail.com>
2026-06-11 23:08:42 +0530

    SQL

---
+++ SQL/Selecting data from table.sql
@@ -0,0 +1,22 @@
+-- USE schooldb;
+
+-- SELECT * FROM student WHERE grade = '10th';
+
+-- SELECT * FROM student WHERE age > 16;
+
+-- SELECT * FROM student WHERE grade IS NULL;
+
+-- SELECT * FROM student WHERE grade IS NOT NULL;
+
+-- SELECT * FROM student WHERE grade = '10th' AND age > 16;
+
+-- SELECT * FROM student WHERE grade = '9th' OR grade = '12th';
+
+-- SELECT * FROM student
+-- WHERE (grade = '10th' OR grade = '11th') AND age >= 16;
+
+-- SELECT * FROM student ORDER BY age ASC;
+
+-- SELECT * FROM student ORDER BY name DESC;
+
+-- SELECT * FROM student LIMIT 2, 5;
